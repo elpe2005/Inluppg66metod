ee9ee126e0d1a62e94452b2986edf12606450363
Oscar05001 <oskr05001@utb.vaxjo.se>
2023-02-15 12:09:57 +0100

    Add project files.

---
+++ Inluppg66metod/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace inluppg63metod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Text("Detta är ett exempel. Texten ändras när den skrivs ut");
+        }
+        static void Text(string lbokstav)
+        {
+            bool test = true;
+            for (int i = 0; i < lbokstav.Length; i++)
+            {
+
+                if (test)
+                {
+                    if (lbokstav[i] == ' ')
+                    {
+                        test = false;
+                        Console.Write(" ");
+                        continue;
+                    }
+                    else 
+                    {
+                        Console.Write(lbokstav.ToUpper()[i]);
+                    }
+                }
+                if (test==false)
+                {
+                    if (lbokstav[i] == ' ')
+                    {
+                        test = true;
+                        Console.Write(" ");
+                        continue;
+                    }
+                    else
+                    {
+                        Console.Write(lbokstav.ToLower()[i]);
+                    }
+                }
+            }   
+        }
+    }
+}
